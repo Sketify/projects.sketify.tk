869849ce6304134207f5d2ea0cef8d009c69662e
Sketify <sketifyofficial@gmail.com>
2021-02-14 14:29:50 +0000

    Update Readnew.js

--- js/Readnew.js
+++ js/Readnew.js
@@ -18,21 +18,26 @@ function getdata() {
 		//now show them in our html
 
 
-		document.getElementById("name").innerHTML = "<strong>Project ID Is Invalid </strong>";
 
-		document.getElementById("country").src = "https://www.pngitem.com/pimgs/b/130-1308065_caution-sign-png.png"; 
-
-	
-
-	// Check If Valid (or) Invalid
-
-		if (name.includes("undefined")) {
 
 			document.getElementById("name").innerHTML = name;
 
 			document.getElementById("gender").innerHTML = gender;
 
 			document.getElementById("country").src = country;
+		
+
+	// Check If Valid (or) Invalid
+
+		if (name.includes("undefined")) {
+
+
+document.getElementById("name").innerHTML = "<strong>Project ID Is Invalid </strong>";
+
+		document.getElementById("country").src = "https://www.pngitem.com/pimgs/b/130-1308065_caution-sign-png.png"; 
+
+document.getElementById("gender").innerHTML = "";	
+
 
 		};
 
