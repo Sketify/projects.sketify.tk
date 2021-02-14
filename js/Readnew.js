a56c371cc97ab1b8771a95d5c8660216d0210d04
Sketify <sketifyofficial@gmail.com>
2021-02-14 15:40:37 +0000

    old pack

--- js/Readnew.js
+++ js/Readnew.js
@@ -1,64 +1,33 @@
 function getdata() {
-	var user = document.getElementById("user").value;
+    var user=document.getElementById("user").value;
+    //firebase data retrieval function
+    //path of your data
+    //.once will get all your data in one time
+    firebase.database().ref('data1/'+user).once('value').then(function (snapshot) {
+        //here we will get data
+        //enter your field name
+        var name=snapshot.val().title;
+        var gender=snapshot.val().name;
+        var country=snapshot.val().icon;
 
-	alert(user);
-	//firebase data retrieval function
-	//path of your data
-	//.once will get all your data in one time
-
-
-	firebase.database().ref('data1/' + user).once('value').then(function (snapshot) {
-		//here we will get data
-
-
-		//enter your field name
-		var name = snapshot.val().title;
-		var gender = snapshot.val().name;
-		var country = snapshot.val().icon;
-
-		//now we have data in variables
-		//now show them in our html
-
-
-
-
-document.getElementById("name").innerHTML = "<strong>Project ID Is Invalid </strong>";
-
-		document.getElementById("country").src = "https://www.pngitem.com/pimgs/b/130-1308065_caution-sign-png.png"; 
-
-document.getElementById("gender").innerHTML = "";	
-
-
-
-	// Check If Valid (or) Invalid
-
-if(o.myProperty === undefined) {
-  alert("myProperty value is the special value `undefined`");
-
-document.getElementById("name").innerHTML = name;
-
-			document.getElementById("gender").innerHTML = gender;
-
-			document.getElementById("country").src = country;
-		
-
-
-
-
-}
-
-			
-
-
-		/*
-
-		var img = document.createElement('img')
-
-		//  img.src 
-
-		= country
-
-		//  document.body.appendChild(img)
-*/
-	})
+        //now we have data in variables
+        //now show them in our html
+        
+         document.getElementById("name").innerHTML = "Link Code was Invalid";
+        
+        if (name.includes("undefined")) {
+        
+        document.getElementById("name").innerHTML = name;
+        document.getElementById("gender").innerHTML = gender;
+         document.getElementById("country").src = country;
+        
+        };
+        
+       // var img = document.createElement('img')
+        
+      //  img.src = country
+        
+      //  document.body.appendChild(img)
+        
+    })
 }
