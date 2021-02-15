8c538843d4ac700a2b813724a1dddb13c0bf8c9e
Sketify <sketifyofficial@gmail.com>
2021-02-15 05:39:52 +0000

    Update Read.js

--- Read.js
+++ Read.js
@@ -28,10 +28,12 @@ function getdata() {
         
       
       if (name.includes("undefined") == false) {
+        
         document.getElementById("country").src = "error.png"; 
-document.getElementById("name").innerHTML = name;
+        document.getElementById("name").innerHTML = name;
         document.getElementById("gender").innerHTML = gender;
-         document.getElementById("country").src = icon;
+        document.getElementById("country").src = icon;
+
       } else {
           
     }
