27b1b882826d6800ca6ad1c9f1ca46fc64071234
Sketify <sketifyofficial@gmail.com>
2021-03-10 06:07:04 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -15,11 +15,6 @@
          
          firebase.initializeApp(firebaseConfig);
 
-            var url_string = window.location.href;
-            var url = new URL(url_string);
-            var a = document.getElementById('btn_open_project');
-            a.href = "projects.sketify://" + url;
-
 
          function openLink() {
 
