dd5b2abd69e19ad544019157e79d3a9cf2774cb3
Sketify <78920165+Sketify@users.noreply.github.com>
2021-04-01 13:11:32 +0530

    Update upload_file.php

--- upload_file.php
+++ upload_file.php
@@ -7,26 +7,25 @@ header("Access-Control-Allow-Origin: *");
 $servername = "remotemysql.com";
 
 //Your DB username
-$username = "8dh3A9nVCE";
+$username = "Qm9FfsRJNG";
 
 //Your DB password
-$password = "8iP0UgXG9X";
+$password = "zSsNGEQFAx";
 
 //Your DB name, required if you have two DB and want to connect to a specific one
-$dbname = "8dh3A9nVCE";
+$dbname = "Qm9FfsRJNG";
 
 //Connect to MySQL
 $mysql = mysqli_connect($servername, $username, $password, $dbname);
 
 if ($mysql->connect_error) {
+  
   echo "Connection failed: " . $mysql->connect_error;
-} else {
-echo "Connected successfully\n";
-}
 
+} else {
 
 //This folder will contain your all uploaded files
-$target_path = "uploads/";
+$target_path = "project_files/";
 
 //Now this contain a path with your filename
 $target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
@@ -41,10 +40,6 @@ $mysql->query($table);
 
 $filepath = $target_path;
 
-//This will check that the file already exists in the folder or not
-if(file_exists($target_path)){
-    echo "file already exists with a same name";
-} else {
 
 //This will upload file to the path
 if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
@@ -57,13 +52,15 @@ if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
     $sql = "INSERT INTO files (filename) VALUES ('$filename')";
     if ($mysql->query($sql) === TRUE) {
     echo "New record created successfully<br>";
-       echo 'https://mskdevyt.000webhostapp.com/Tut/uploads/'.$filename ;
+       echo 'http://projects.sketify.tk/project_files/'.$filename ;
     
 } else {
     echo "Error: " . $sql . "<br>" . $mysql->error;
 }
 } else{
-    echo "There was an error uploading the file, please try again!";
+    echo "Error Uploading";
 }
+  
 }
+
 ?>
