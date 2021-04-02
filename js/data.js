6ceb836f6e4e487ae6c56d1b4e7c74e9924f4539
Sketify <78920165+Sketify@users.noreply.github.com>
2021-04-02 11:18:47 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -17,9 +17,8 @@
 
 
 
-            var url_string = window.location.href;
-            var url = new URL(url_string);
-            var a = document.getElementById('btn_open_project');
-            a.href = "project.sketify://" + url;
-            
+			var url_string = window.location.href;
+			var url = new URL(url_string);
+			var a = document.getElementById('link_main');
+			a.href = "project.sketify://" + url;
 
