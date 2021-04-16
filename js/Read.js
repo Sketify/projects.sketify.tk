8da98b74507ee9975c8a953e80fb7cb8f9d25845
Sketify <sketifyofficial@gmail.com>
2021-04-16 13:30:29 +0100

    upd

--- js/Read.js
+++ js/Read.js
@@ -1,67 +1,61 @@
-
-
 function getdata() {
 
-      
-    
-    
-    var url1 = window.location.href;
 
-    var url2 = url1.replace("https://projects.sketify.tk/project?id=prefix-9a71e0719545676a138b25f1a00c30e4dfba301d?&&", "")
-    
-    var id = url2.replace("&&afl=https://sketify.tk&efr=1", "")
+	var url1 = window.location.href;
+
+	var url2 = url1.replace("https://projects.sketify.tk/project?id=prefix-9a71e0719545676a138b25f1a00c30e4dfba301d?&&", "")
+
+	var id = url2.replace("&&afl=https://sketify.tk&efr=1", "")
 
 
-        ///////////////////////////////////////////// ////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////// ////////////////////////////////////////////////////////////////
 
-        // ============================================= Main Header ==============================================
+	// ============================================= Main Header ==============================================
 
-           
 
-        // ============================================= Main Header End ==========================================
+	// ============================================= Main Header End ==========================================
 
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-        // ============================================== Main Body Start =========================================
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        // Send Request to Database To Search ID
+	// ============================================== Main Body Start =========================================
 
-        firebase.database().ref('data1/'+id).once('value').then(function (snapshot) {
-        
-        // Set Data To Variaables
+	// Send Request to Database To Search ID
 
-            var name=snapshot.val().title;
+	firebase.database().ref('data1/' + id).once('value').then(function (snapshot) {
 
-            var gender=snapshot.val().name;
+		// Set Data To Variaables
 
-            var icon=snapshot.val().icon;
-    
-        // Variables Set
-        
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
- // Set Error 
-  
-            document.getElementById("name").innerHTML = "<strong>  Project ID Is Invalid </strong>(Check The Url)";
+		var name = snapshot.val().title;
 
-         //   document.getElementById("country").src = "error.png"; 
-      
-        if (name.includes("undefined") == false) {
+		var gender = snapshot.val().name;
 
-            document.getElementById("country").src = "transpa.png";
-            document.getElementById("country").src = icon;
-            document.getElementById("name").innerHTML = "   " + name;
-            document.getElementById("gender").innerHTML = gender;           
-            document.getElementById("heading").innerHTML = name + " - Sketify";
-            document.getElementById("des").innerHTML = name + " shared a Project on Sketify";
-        }
+		var icon = snapshot.val().icon;
 
-        // ============================================== Main Body End ===========================================
+		// Variables Set
 
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-      
-        }
-    )
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		// Set Error 
+
+		document.getElementById("name").innerHTML = "<strong>  Project ID Is Invalid </strong>(Check The Url)";
+
+		//   document.getElementById("country").src = "error.png"; 
+
+		if (name.includes("undefined") == false) {
+
+			document.getElementById("country").src = "transpa.png";
+			document.getElementById("country").src = icon;
+			document.getElementById("name").innerHTML = "   " + name;
+			document.getElementById("gender").innerHTML = gender;
+			document.getElementById("heading").innerHTML = name + " - Sketify";
+			document.getElementById("des").innerHTML = name + " shared a Project on Sketify";
+		}
+
+		// ============================================== Main Body End ===========================================
+
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	})
 }
 
-        getdata();
+getdata();
