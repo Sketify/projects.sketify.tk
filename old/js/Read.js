4c5438b835b83dd21823b8f498b30fdf7c07eada
Sketify <sketifyofficial@gmail.com>
2021-04-21 19:51:23 +0100

    upd

---
+++ old/js/Read.js
@@ -0,0 +1,69 @@
+function getdata() {
+
+
+	/*  
+    
+    var str = window.location.href;
+
+    var str1 = url1.replace("https://projects.sketify.tk/project?id=prefix-9a71e0719545676a138b25f1a00c30e4dfba301d?&&", "")
+    
+    var id = url2.replace("&&afl=https://sketify.tk&efr=1", "")
+
+*/
+
+	const urlParams = new URLSearchParams(window.location.search);
+
+	const id = urlParams.get('id');
+
+
+	///////////////////////////////////////////// ////////////////////////////////////////////////////////////////
+
+	// ============================================= Main Header ==============================================
+
+
+	// ============================================= Main Header End ==========================================
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// ============================================== Main Body Start =========================================
+
+	// Send Request to Database To Search ID
+
+	firebase.database().ref('data1/' + id).once('value').then(function (snapshot) {
+
+		// Set Data To Variaables
+
+		var name = snapshot.val().title;
+
+		var gender = snapshot.val().name;
+
+		var icon = snapshot.val().icon;
+
+		// Variables Set
+
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		// Set Error 
+
+		document.getElementById("name").innerHTML = "<strong>  Project ID Is Invalid </strong>";
+
+		//   document.getElementById("country").src = "error.png"; 
+
+		if (name.includes("undefined") == false) {
+
+			document.getElementById("country").src = "transpa.png";
+			document.getElementById("country").src = icon;
+			document.getElementById("name").innerHTML = "   " + name;
+			document.getElementById("gender").innerHTML = gender;
+			document.getElementById("heading").innerHTML = name + " - Sketify";
+			document.getElementById("des").innerHTML = name + " shared a Project on Sketify";
+		}
+
+		// ============================================== Main Body End ===========================================
+
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	})
+}
+
+getdata();
