496cef17530d62c685afc51d384b9c491048c49a
Sketify <sketifyofficial@gmail.com>
2021-05-09 00:02:33 +0100

    main_update

--- js/check.js
+++ js/check.js
@@ -1,3 +1,24 @@
+<script>
+
+
+         // Your web app's Firebase configuration
+         
+         var firebaseConfig = {
+             apiKey: "AIzaSyBK-juZ6krPJCHHELQgOW9sFUXsS9h3wHI",
+             authDomain: "sketify-f7280.firebaseapp.com",
+             databaseURL: "https://sketify-f7280-default-rtdb.firebaseio.com/",
+             projectId: "sketify-f7280-default-rtdb",
+             storageBucket: "sketify-f7280.appspot.com",
+             messagingSenderId: "463332404757",
+             appId: "1:529893568319:android:3362cdc8bbdeb17c486d8a"
+         };
+         
+         // Initialize Firebase
+         
+         firebase.initializeApp(firebaseConfig);
+
+
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
@@ -58,3 +79,5 @@ function login(){
 function logout(){
   firebase.auth().signOut();
 }
+
+</script>
--- js/data.js
+++ js/data.js
@@ -1,21 +1,4 @@
 
-         // Your web app's Firebase configuration
-         
-         var firebaseConfig = {
-             apiKey: "AIzaSyBK-juZ6krPJCHHELQgOW9sFUXsS9h3wHI",
-             authDomain: "sketify-f7280.firebaseapp.com",
-             databaseURL: "https://sketify-f7280-default-rtdb.firebaseio.com/",
-             projectId: "sketify-f7280-default-rtdb",
-             storageBucket: "sketify-f7280.appspot.com",
-             messagingSenderId: "463332404757",
-             appId: "1:529893568319:android:3362cdc8bbdeb17c486d8a"
-         };
-         
-         // Initialize Firebase
-         
-         firebase.initializeApp(firebaseConfig);
-
-
 
             var url_string = window.location.href;
             var url = new URL(url_string);
